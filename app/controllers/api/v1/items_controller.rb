4d0f641493c5d794585afe818eb3477163b71357
Yohei Senju <yohei.senju@gmail.com>
2017-01-04 16:41:47 +0900

    +1 Implement items internal api

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::ItemsController < Api::V1::BaseController
+  def index
+    respond_with Item.all
+  end
+
+  def create
+    respond_with :api, :v1, Item.create(permit_params)
+  end
+
+  def destroy
+    respond_with Item.destroy(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update item_params
+    respond_with item, json: item
+  end
+
+  private def permit_params
+     params.
+       require(:item).
+         permit(
+           :id,
+           :name,
+           :description
+         )
+  end
+end
